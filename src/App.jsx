fefabbf8db54f641712abaa01709f2d0857b7024
Shaharyar Nasir <95301226+ShaharyarNasir@users.noreply.github.com>
2025-07-31 20:06:58 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
