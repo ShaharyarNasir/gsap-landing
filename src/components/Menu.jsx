6b452bc9d42e52500ad3ea32334309cbed73b8c6
Nimra Nasir <mshaharyarnasir@gmail.com>
2025-07-31 18:37:42 +0500

    app.jsx deploy fix & menu/slider working on it

---
+++ src/components/Menu.jsx
@@ -0,0 +1,50 @@
+import { allCocktails } from "../../constants";
+import { useState } from "react";
+import { gsap } from "gsap";
+
+const Menu = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalCocktails = allCocktails.length;
+
+  const goToSlider = (index) => {
+    const newIndex = (index + totalCocktails) % totalCocktails;
+
+    setCurrentIndex(newIndex);
+   }
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+      <img
+        src="/images/slider-left-leaf.png"
+        alt="left leaf"
+        id="m-left-leaf"
+      />
+      <img
+        src="/images/slider-right-leaf.png"
+        alt="right leaf"
+        id="m-right-leaf"
+      />
+
+      <h2 id="menu-heading" className="sr-only">
+        Cocktail Menu
+      </h2>
+
+      <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
+        {allCocktails.map((cocktail, index) => {
+          const isActive = index === currentIndex;
+
+          return (
+            <button key={cocktail.id} className={`
+            ${isActive 
+            ? "text-white border-white" 
+            : "text-white/50 border-white/50"}`
+            } onClick={() => goToSlider(index)}>
+                {cocktail.name}
+            </button>
+          )
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Menu;
